a36664ee37036a9869fce8899379dcfc376e49aa
Joel Oliveira <joel.oliveira@gmail.com>
2016-07-23 19:45:54 -0400

    Add initial zsh dotfiles

---
+++ tag-zsh/config/zsh/aliases.zsh
@@ -0,0 +1,8 @@
+alias g='git'
+
+alias Grep='grep'
+
+# Alias pbcopy and pbpaste to match the utilities in OSX. Lifted from -
+# https://superuser.com/questions/288320/whats-like-osxs-pbcopy-for-linux
+alias pbcopy='xsel --clipboard --input'
+alias pbpaste='xsel --clipboard --output'
