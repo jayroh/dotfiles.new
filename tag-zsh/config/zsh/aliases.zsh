8d602950d8ffff551ed49378e053ab06674df217
Joel Oliveira <joel.oliveira@gmail.com>
2016-07-23 22:18:03 -0400

    glog and reload! aliases
    `glog` because muscle memory, oops.
    
    `reload!` because that's much shorter and requires less digital
    dexterity

--- tag-zsh/config/zsh/aliases.zsh
+++ tag-zsh/config/zsh/aliases.zsh
@@ -1,6 +1,10 @@
 alias g='git'
 
+alias glog='g l'
+
 alias Grep='grep'
+
+alias reload!='source ~/.zshrc'
 
 # Alias pbcopy and pbpaste to match the utilities in OSX. Lifted from -
 # https://superuser.com/questions/288320/whats-like-osxs-pbcopy-for-linux
