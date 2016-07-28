469833103da16ffb2d378c6745bcf2cd95a19fd6
Joel Oliveira <joel.oliveira@gmail.com>
2016-07-27 23:42:01 -0400

    Better solution to the ssh-agent selection problem

---
+++ tag-zsh/config/zsh/ssh.zsh
@@ -0,0 +1,9 @@
+# If authorized_keys does not exist, create it
+[[ ! -f ~/.ssh/authorized_keys ]] && touch ~/.ssh/authorized_keys
+
+if [[ ! -S ~/.ssh/ssh_auth_sock ]]; then
+  eval `ssh-agent`
+  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+fi
+export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+ssh-add -l | grep "The agent has no identities" && ssh-add
