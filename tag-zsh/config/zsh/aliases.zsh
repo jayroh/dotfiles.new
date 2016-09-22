f5fd81d071be3d82fda158d6ca149a2bc565c0e2
Joel Oliveira <joel.oliveira@gmail.com>
2016-09-22 11:23:38 -0400

    Better rcup and rcupall
    The recent rcup alias bothered me, and an alias to do something akin to
    `rcup -all` is needed more and better anyway. This new alias will pipe
    all tags in my dotfiles and add the `-t` option to make rcup update all
    at once.

--- tag-zsh/config/zsh/aliases.zsh
+++ tag-zsh/config/zsh/aliases.zsh
@@ -12,11 +12,10 @@ alias Grep='grep'
 
 alias hist='history'
 
-alias rcup='rcup -x setup -t tag-zsh -t tag-nvim'
-
+alias rcup='rcup -x setup'
+alias rcupall="rcup `ls ~/.dotfiles | grep tag | sed 's/^tag-/-t /' | tr "\n" ' '`"
 alias reload_zsh='source ~/.zshrc'
-alias reload_rcup='ls ~/.dotfiles/ | cut -d- -f2 | xargs rcup -t'
-alias reload!='reload_zsh && reload_rcup'
+alias reload!='reload_zsh && rcupall'
 
 # Alias pbcopy and pbpaste to match the utilities in OSX. Lifted from -
 # https://superuser.com/questions/288320/whats-like-osxs-pbcopy-for-linux
