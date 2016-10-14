5f3ff756360d5267673e7b82bd659169ed5db39e
Joel Oliveira <joel.oliveira@gmail.com>
2016-10-14 09:00:56 -0400

    Set $JAVA_HOME and add jre/bin to $PATH
    This is mostly to get "wrk" to work.
    
    ... work work work work work
    
    ![](https://media2.giphy.com/media/inVvfuomoD31K/200_s.gif)

--- tag-zsh/config/zsh/path.zsh
+++ tag-zsh/config/zsh/path.zsh
@@ -1,2 +1,5 @@
+export JAVA_HOME=$(/usr/libexec/java_home)
+
 path=( "/usr/bin" "/bin" "/usr/sbin" "/sbin" )
 path=( "$HOME/.bin" "$HOME/.rbenv/bin" "/usr/local/bin" $path )
+path=( "$JAVA_HOME/jre/bin" $path )
