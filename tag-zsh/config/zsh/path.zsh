884ff4999646dc80e44620fa53d5ecc72bcd3a25
Joel Oliveira <joel.oliveira@gmail.com>
2016-12-11 16:28:49 -0500

    `java_home` only exists in OSX I think?
    If that's the case then check for another directory that can be
    considered $JAVA_HOME

--- tag-zsh/config/zsh/path.zsh
+++ tag-zsh/config/zsh/path.zsh
@@ -1,4 +1,5 @@
-export JAVA_HOME=$(/usr/libexec/java_home)
+[[ -d /usr/lib/jvm/java-8-openjdk-amd64 ]] &&  export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+[[ -x /usr/libexec/java_home ]] &&  export JAVA_HOME=$(/usr/libexec/java_home)
 
 path=( "/usr/bin" "/bin" "/usr/sbin" "/sbin" )
 path=( "$HOME/.bin" "$HOME/.rbenv/bin" "/usr/local/bin" $path )
