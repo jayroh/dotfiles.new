8865a1882f7e692683187111667cdde8f52571a3
Joel Oliveira <joel.oliveira@gmail.com>
2017-01-07 16:36:14 -0500

    Add node_modules/.bin to path
    Gets node/yarn project bins to work

--- tag-zsh/config/zsh/path.zsh
+++ tag-zsh/config/zsh/path.zsh
@@ -2,5 +2,5 @@
 [[ -x /usr/libexec/java_home ]] &&  export JAVA_HOME=$(/usr/libexec/java_home)
 
 path=( "/usr/bin" "/bin" "/usr/sbin" "/sbin" )
-path=( "$HOME/.bin" "$HOME/.rbenv/bin" "/usr/local/bin" $path )
+path=( "node_modules/.bin" "$HOME/.bin" "$HOME/.rbenv/bin" "/usr/local/bin" $path )
 path=( "$JAVA_HOME/jre/bin" $path )
