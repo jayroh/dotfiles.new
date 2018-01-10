60fb970beac1f85f1ae17b531f2f07c241773a86
Joel Oliveira <joel.oliveira@gmail.com>
2018-01-09 20:43:43 -0500

    Starting with ZSH completion

---
+++ tag-zsh/config/zsh/completion.zsh
@@ -0,0 +1,12 @@
+# Point fpath to our own completion scripts directory
+
+fpath=(~/.config/zsh/completion $fpath)
+
+autoload -U compinit
+compinit
+
+# matches case insensitive for lowercase
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# pasting with tabs doesn't perform completion
+zstyle ':completion:*' insert-tab pending
