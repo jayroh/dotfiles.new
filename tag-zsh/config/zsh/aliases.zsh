d6d4b69c2fb82cb2a8448380d94b7e25d7b07bce
Joel Oliveira <joel.oliveira@gmail.com>
2018-02-19 17:15:52 -0500

    Change order of reloading actions
    reloading zsh and THEN updating all the rc files is backwards - we
    should update all rc files THEN reload.

--- tag-zsh/config/zsh/aliases.zsh
+++ tag-zsh/config/zsh/aliases.zsh
@@ -27,7 +27,7 @@ alias gpom='g pull origin master'
 alias rcup='rcup -x setup'
 alias rcupall="rcup `ls ~/.dotfiles | grep tag- | sed 's/^tag-/-t /' | tr "\n" ' '`"
 alias reload_zsh='source ~/.zshrc'
-alias reload!='reload_zsh && rcupall'
+alias reload!='rcupall; reload_zsh'
 
 # If need be, alias pbcopy and pbpaste to match the utilities in OSX. Lifted
 # from - https://superuser.com/questions/288320/whats-like-osxs-pbcopy-for-linux
@@ -47,3 +47,11 @@ alias t-reload='sudo service transmission-daemon reload'
 alias t-list="transmission-remote -n '$TRANSMISSION_USER_NAME:$TRANSMISSION_USER_PASS' -l"
 alias t-basicstats="transmission-remote -n '$TRANSMISSION_USER_NAME:$TRANSMISSION_USER_PASS' -st"
 alias t-fullstats="transmission-remote -n '$TRANSMISSION_USER_NAME:$TRANSMISSION_USER_PASS' -si"
+
+function t-info() {
+  if [ "$1" != "" ]; then
+  transmission-remote -t $1 -n $TRANSMISSION_USER_NAME:$TRANSMISSION_USER_PASS -i
+  else
+  echo "nope"
+  fi
+}
