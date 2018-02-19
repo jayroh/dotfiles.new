475529f95a72d712f396c0c0b8c487604b137a17
Joel Oliveira <joel.oliveira@gmail.com>
2018-02-19 17:10:25 -0500

    Set node up to install global packages in home dir

---
+++ tag-zsh/config/zsh/node.zsh
@@ -0,0 +1,18 @@
+if [[ -d "${HOME}/.nvm" ]]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"  # This loads nvm
+fi
+
+if [[ -d "${HOME}/.yarn/bin" ]]; then
+  export PATH="${HOME}/.yarn/bin:$PATH"
+fi
+
+if [[ ! -d "${HOME}/.npm-packages" ]]; then
+  mkdir "${HOME}/.npm-packages"
+fi
+
+export NPM_PACKAGES="${HOME}/.npm-packages"
+
+export PATH="${NPM_PACKAGES}/bin:${PATH}"
+
+export MANPATH="${NPM_PACKAGES}/share/man:$(manpath)"
