cb3751790ddf20e45d460fb0c511104410721eaf
Joel Oliveira <joel.oliveira@gmail.com>
2019-02-13 18:23:03 -0500

    Move Postgres.app cli path to front of path list
    Get rid of linting

--- tag-nvim/vim/config/linting.vim
+++
@@ -1,6 +0,0 @@
-let g:neomake_javascript_enabled_makers = ['standard']
-autocmd BufWritePost *.js,*.es6 silent !standard-format -w %
-autocmd! BufReadPost,BufWritePost * Neomake
-
-let g:prettier#autoformat = 0
-autocmd BufWritePre *.ts,*.js,*.es6 PrettierAsync
--- tag-zsh/config/zsh/path.zsh
+++ tag-zsh/config/zsh/path.zsh
@@ -2,11 +2,11 @@
 [[ -x /usr/libexec/java_home ]]            &&  export JAVA_HOME=$(/usr/libexec/java_home)
 
 path=( "/usr/bin" "/bin" "/usr/sbin" "/sbin" )
-path=( "/Applications/Postgres.app/Contents/Versions/latest/bin" $path )
 path=( "$HOME/.npm-packages/bin" "node_modules/.bin" "$HOME/.bin" "$HOME/.rbenv/bin" "/usr/local/bin" $path )
 path=( "/usr/local/opt/elasticsearch@2.4/bin" $path )
 path=( "$JAVA_HOME/jre/bin" $path )
 path=( "/usr/lib/go-1.8/bin" $path )
+path=( "/Applications/Postgres.app/Contents/Versions/latest/bin" $path )
 path=( "/usr/local/opt/imagemagick@6/bin" $path )
 
 [[ -d $HOME/Qt5.5.1/5.5/clang_64/bin ]] && path=( "$HOME/Qt5.5.1/5.5/clang_64/bin" $path )
