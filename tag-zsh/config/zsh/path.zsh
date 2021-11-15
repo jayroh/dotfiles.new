1f88c513cad3723d3069d4d1a9bc22ef591abb59
Joel Oliveira <joel.oliveira@gmail.com>
2021-11-15 17:46:01 -0500

    Further tighten up the setup script
    Check for presence of dirs and files before trying to download or
    clone repos.
    
    Change the Brewfile to use `postgres-unofficial` instead of `postgres`
    
    Move the brew bin path to the front of $PATH

--- tag-zsh/config/zsh/path.zsh
+++ tag-zsh/config/zsh/path.zsh
@@ -7,13 +7,14 @@ fi
 [[ -d /usr/lib/jvm/java-8-openjdk-amd64 ]] &&  export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 [[ -x /usr/libexec/java_home ]]            &&  export JAVA_HOME=$(/usr/libexec/java_home)
 
-path=( "/usr/bin" "/bin" "/usr/sbin" "/sbin" "$HOMEBREW_HOME/bin")
+path=( "/usr/bin" "/bin" "/usr/sbin" "/sbin" )
 path=( "$HOME/.npm-packages/bin" "node_modules/.bin" "$HOME/.bin" "$HOME/.rbenv/bin" "/usr/local/bin" $path )
 path=( "$HOME/.npm-packages/bin" "node_modules/.bin" "$HOME/.bin" "$HOME/.rbenv/bin" "/usr/local/bin" $path )
 path=( "$HOME/.local/bin" $path )
 path=( "$JAVA_HOME/jre/bin" $path )
 path=( "$HOMEBREW_HOME/opt/imagemagick@6/bin" $path )
 path=( "/Applications/Postgres.app/Contents/Versions/latest/bin" $path )
+path=( "$HOMEBREW_HOME/bin" $path )
 
 [[ -d $HOME/Qt5.5.1/5.5/clang_64/bin ]] && path=( "$HOME/Qt5.5.1/5.5/clang_64/bin" $path )
 
@@ -21,5 +22,5 @@ if [[ -d $HOMEBREW_HOME/opt/openssl/bin ]]; then
   path=( "$HOMEBREW_HOME/opt/openssl/bin" $path )
   export LDFLAGS=-L$HOMEBREW_HOME/opt/openssl/lib
   export CPPFLAGS=-I$HOMEBREW_HOME/opt/openssl/include
-  export PKG_CONFIG_PATH=$HOMEBREW_HOME/opt/openssl/lib/pkgconfig
+  export PKG_CONFIG_PATH=$HOMEBREW_HOME/openssl/lib/pkgconfig
 fi
