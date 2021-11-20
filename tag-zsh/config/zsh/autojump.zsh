5c95bd3d5d9893bc3523c695fec598c7752616b6
Joel Oliveira <joel.oliveira@gmail.com>
2021-11-20 08:43:44 -0500

    Source autojump when on an m1 mac

--- tag-zsh/config/zsh/autojump.zsh
+++ tag-zsh/config/zsh/autojump.zsh
@@ -5,3 +5,8 @@
 # if autojump exists on a mac
 [[ -s /usr/local/bin/brew ]] && \
   . `brew --prefix`/etc/autojump.sh
+
+# if autojump exists on an M1 mac
+[[ -s /opt/homebrew/share/autojump/autojump.zsh ]] && \
+  source /opt/homebrew/share/autojump/autojump.zsh
+
