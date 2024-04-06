c76cec40bf0e1ceebb75c053cc6494e3039ce392
Joel Oliveira <joel.oliveira@gmail.com>
2024-04-06 14:17:58 -0400

    Clean up all path stuff

--- tag-zsh/config/zsh/fzf.zsh
+++ tag-zsh/config/zsh/fzf.zsh
@@ -1,19 +1,10 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/joel/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/joel/.fzf/bin"
-fi
-
-# Man path
-# --------
-if [[ ! "$MANPATH" == */home/joel/.fzf/man* && -d "/home/joel/.fzf/man" ]]; then
-  export MANPATH="$MANPATH:/home/joel/.fzf/man"
-fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/joel/.fzf/shell/completion.zsh" 2> /dev/null
+source "/opt/homebrew/opt/fzf/shell/completion.zsh"
 
 # Key bindings
 # ------------
-[[ -f "/home/joel/.fzf/shell/key-bindings.zsh" ]] && source "/home/joel/.fzf/shell/key-bindings.zsh"
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
--- tag-zsh/config/zsh/path.zsh
+++ tag-zsh/config/zsh/path.zsh
@@ -1,17 +1,35 @@
 [[ -d /usr/lib/jvm/java-8-openjdk-amd64 ]] && export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 [[ -x /usr/libexec/java_home ]]            && export JAVA_HOME=$(/usr/libexec/java_home)
-[[ -d "$HOME/.yarn/bin" ]]                 && export PATH="$HOME/.yarn/bin:$PATH"
 [[ -d "$HOME/.nvm" ]]                      && export NVM_DIR="$HOME/.nvm"
+[[ -d "$HOME/.yarn/bin" ]]                 && export PATH="$HOME/.yarn/bin:$PATH"
 
-path=( "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/snap/bin" )
-path=( "$HOME/.npm-packages/bin" "node_modules/.bin" "$HOME/.bin" "/usr/local/bin" $path )
-path=( "$HOME/.local/bin" $path )
-path=( "$JAVA_HOME/jre/bin" $path )
-[[ -d $HOME/Qt5.5.1/5.5/clang_64/bin ]] && path=( "$HOME/Qt5.5.1/5.5/clang_64/bin" $path )
+path=(
+	"/usr/bin"
+	"/bin"
+	"/usr/sbin"
+	"/sbin"
+	"/snap/bin"
+	"$HOME/.npm-packages/bin"
+	"node_modules/.bin"
+	"$HOME/.bin"
+	"/usr/local/bin"
+	"$HOME/.local/bin"
+	"$JAVA_HOME/jre/bin"
+)
+
+QT_BIN="$HOME/Qt5.5.1/5.5/clang_64/bin"
+[[ -d  $QT_BIN ]] && path=(
+	$QT_BIN
+	$path
+)
 
 PG_APP_BIN="/Applications/Postgres.app/Contents/Versions/latest/bin"
-[[ -d $PG_APP_BIN ]] && path=( $PG_APP_BIN $path )
+[[ -d $PG_APP_BIN ]] && path=(
+	$PG_APP_BIN
+	$path
+)
 
+# Set up all homebrew paths
 if [ "$(uname -m 2> /dev/null)" = "arm64" ]; then
   export HOMEBREW_HOME="/opt/homebrew"
 else
@@ -19,14 +37,22 @@ else
 fi
 
 if [[ -f "$HOMEBREW_HOME/bin/brew" ]] &> /dev/null; then
-  path=( "$HOMEBREW_HOME/bin" "$HOMEBREW_HOME/sbin" $path )
-  path=( "$HOMEBREW_HOME/opt/imagemagick@6/bin" $path )
-  path=( "$HOMEBREW_HOME/opt/bison@2.7)/bin" $path )
+	path=(
+		"$HOMEBREW_HOME/bin"
+		"$HOMEBREW_HOME/sbin"
+		"$HOMEBREW_HOME/opt/imagemagick@6/bin"
+		"$HOMEBREW_HOME/opt/bison@2.7/bin"
+		$path
+	)
 
-  if [[ -d $HOMEBREW_HOME/opt/openssl/bin ]]; then
-    path=( "$HOMEBREW_HOME/opt/openssl/bin" $path )
-    export LDFLAGS="-L$HOMEBREW_HOME/opt/openssl/lib"
-    export CPPFLAGS="-I$HOMEBREW_HOME/opt/openssl/include"
-    export PKG_CONFIG_PATH="$HOMEBREW_HOME/openssl/lib/pkgconfig"
-  fi
+	# Set up openssl path and ENV's
+	if [[ -d $HOMEBREW_HOME/opt/openssl/bin ]]; then
+		path=(
+			"$HOMEBREW_HOME/opt/openssl/bin"
+			$path
+		)
+		export LDFLAGS="-L$HOMEBREW_HOME/opt/openssl/lib"
+		export CPPFLAGS="-I$HOMEBREW_HOME/opt/openssl/include"
+		export PKG_CONFIG_PATH="$HOMEBREW_HOME/openssl/lib/pkgconfig"
+	fi
 fi
