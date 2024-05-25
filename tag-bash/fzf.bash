194a0070f1954aab64b4a614c4c42e07e0ce4833
Joel Oliveira <joel.oliveira@gmail.com>
2024-05-24 21:11:54 -0400

    fixes for debian machine

--- tag-bash/fzf.bash
+++ tag-bash/fzf.bash
@@ -2,12 +2,12 @@
 # ---------
 if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+
+  # Auto-completion
+  # ---------------
+  source "/opt/homebrew/opt/fzf/shell/completion.bash"
+
+  # Key bindings
+  # ------------
+  source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
 fi
-
-# Auto-completion
-# ---------------
-source "/opt/homebrew/opt/fzf/shell/completion.bash"
-
-# Key bindings
-# ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
